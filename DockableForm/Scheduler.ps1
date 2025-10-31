e30bc3e56c7b74e9de9389420886d8bb21278f1e
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-10-31 16:13:49 +0900

    Button Pos Fix

--- DockableForm/Scheduler.ps1
+++ DockableForm/Scheduler.ps1
@@ -3714,7 +3714,7 @@ namespace MetroUI
             int addButtonSize = 48;
             int addButtonMargin = 16;
             int addButtonX = ClientSize.Width - addButtonSize - addButtonMargin;
-            int addButtonY = ClientSize.Height - addButtonSize - addButtonMargin;
+            int addButtonY = ClientSize.Height - addButtonSize - addButtonMargin - 25;
 
             if (addButtonX < addButtonMargin)
                 addButtonX = addButtonMargin;
