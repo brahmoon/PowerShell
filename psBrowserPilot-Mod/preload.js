1bc2ef79dcd6cfc317368dfe6ba694722faa25e4
brahmoon <51511682+brahmoon@users.noreply.github.com>
2025-11-05 16:24:12 +0900

    del

--- psBrowserPilot-Mod/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveGraph: (payload) => ipcRenderer.invoke('graph:save', payload),
-  loadGraph: () => ipcRenderer.invoke('graph:load'),
-  saveScript: (payload) => ipcRenderer.invoke('script:save', payload),
-  selectLocalFile: (payload) => ipcRenderer.invoke('dialog:selectFile', payload),
-});
